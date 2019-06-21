79a9ac58a4ae8123431f09e1ef72b41878e98063
Samaneh Dallalizad <samaneh.dallalizad@gmail.com>
2019-06-21 15:06:54 +0200

    test parameters2

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.post('/hooks',(req,res,next)=>{
   if (req.body.queryResult.action === "menu") {
     res.send({fulfillmentText:"test menu samaneh and drashti"})
     if(req.body.queryResult.parameters.hasOwnProperty("date")){
-      res.json({fulfillmentText:"level 2"})
+      res.send({fulfillmentText:"level 2"})
     }
  
    }

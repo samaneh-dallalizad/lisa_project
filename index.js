1ed6ffdbe84968e883441a2d7d11b1ae07a6d405
Samaneh Dallalizad <samaneh.dallalizad@gmail.com>
2019-06-25 11:08:55 +0200

    change index

--- index.js
+++ index.js
@@ -3,15 +3,11 @@ const app =express()
 const bodyParser=require("body-parser")
 const dishRouter = require("./dish-table/routes");
 const typeRouter = require('./type-table/routes')
-const port =process.env.PORT||4000
+const port =process.env.PORT||5000
 const HooksRouter=require("./routes/hooks")
 const MenuRouter=require("./routes/menu")
 const moment =require("moment")
 const cors = require('cors')
-
-const date="2019-06-25T12:00:00+02:00"
-
-console.log(moment(date).calendar())
 app
   .use(cors())
   .use(bodyParser.json())

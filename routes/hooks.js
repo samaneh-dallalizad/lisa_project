563453740fcc5a24f7b51d54c89d265dcec98b3c
Samaneh Dallalizad <samaneh.dallalizad@gmail.com>
2019-06-25 11:07:20 +0200

    today menu and next week menu

--- routes/hooks.js
+++ routes/hooks.js
@@ -4,35 +4,42 @@ const moment =require("moment")
 const router=new Router();
 
 router.post('/hooks',(req,res,next)=>{ 
-
-    console.table(req.body.queryResult)
-    const {action, parameters} = req.body.queryResult
-   
+    
+    const {action, parameters} = req.body.queryResult  
     
      if (action === "menu") {  
-     // console.log(+moment(parameters.date))
+      // console.log(+moment(parameters.date))
       //today menu 
-       if(parameters.date){      
+       if(parameters.date){ 
+        Menu.aggregate('dish_name', 'DISTINCT', {where:{date:moment().format('YYYY-MM-DD')}, plain: false })
+            .then(menus=>{ 
+                if(menus){
+                  res.send({fulfillmentText:"the menu is :"+menus.map(menu => menu.DISTINCT).join(', ')})
+                }else{
 
-          if(moment(parameters.date).format('MM-DD-YYYY')=== moment().format('MM-DD-YYYY')){  
-          
-            Menu.aggregate('dish_name', 'DISTINCT', {where:{date:moment().format('YYYY-MM-DD')}, plain: false })
-                .then(menus=>{
-                  console.log(menus)                             
-                    res.send({fulfillmentText:"today menu is :"+ menus.map(menu => menu.DISTINCT).join(', ')
-                    +"............"})               
-         })
-          
-        }
-       else if(+moment(parameters.date) < +moment()){
-            res.send("others day")
-        }
-        
+                  res.send({fulfillmentText:"there is no menu yet"})
+                }                                          
+                        
+            }).catch(error=>
+               console.log(error)
+            ) 
          
-      
+      //   if(moment(parameters.date).format('MM-DD-YYYY')=== moment().format('MM-DD-YYYY')){           
+                     
+      //    }
+      //   else if(+moment(parameters.date) > +moment()){
+      //       console.log(moment(parameters.date).format('MM-DD-YYYY'))
+      //       console.log(moment().format('MM-DD-YYYY'))
+      //       res.send("others day")
+      //   }
+      //   else{
+      //     res.send("oops")
+      //   }
 
-       }
-       res.send("oops")
+        }else{
+           res.send("")
+        }      
+         
      }
   })
   module.exports=router
--- routes/menu.js
+++ routes/menu.js
@@ -12,15 +12,13 @@ router.get("/menus",(req,res,next)=>{
   })    
   
  })
-
+ 
  router.post("/menus" , (req,res,next)=>{
   const newMenu={
     type_name:req.body.type_name,
     dish_name:req.body.dish_name,
     date:req.body.date
   }
-
-
   Menu.create(newMenu)
   .then(menu=>res.status(201).json(menu))
 })
